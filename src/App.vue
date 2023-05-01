659faced07d533dd157bc2e907f536a3f79d7214
gometen <gometen@gmail.com>
2023-05-01 04:49:06 +0000

    Add FourierWave

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+      <FourierWave/>
       <HelloWorld/>
     </v-main>
   </v-app>
@@ -8,12 +9,14 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FourierWave from './components/FourierWave.vue'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    FourierWave,
   },
 
   data: () => ({
