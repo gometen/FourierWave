f760b34d4c13390c6e934ed8ab03f120d2eb7a99
gometen <gometen@gmail.com>
2023-05-01 04:54:16 +0000

    Update docs

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <v-app>
     <v-main>
       <FourierWave/>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import FourierWave from './components/FourierWave.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
     FourierWave,
   },
 
