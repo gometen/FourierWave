23791f1e616146904ec297a30bb9b9150784676a
gometen <gometen@gmail.com>
2023-06-20 15:25:39 +0000

    Modified Graph

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -29,15 +29,21 @@
       </v-col>
     </v-row>
     <v-row>
-      <div>
-        <canvas ref="canvas1"></canvas>
-      </div>
+      <v-col cols="12" md="3" lg="3" xl="3">
+        <div align="center">
+          <canvas ref="canvas1"></canvas>
+        </div>
+      </v-col>
+      <v-col cols="12" md="9" lg="9" xl="9">
+        <div id="graph" align="center"></div>
+      </v-col>
     </v-row>
-
   </v-container>
 </template>
 
 <script>
+  import Plotly from 'plotly.js-basic-dist'
+
   // 物体の初期状態を設定
   var position = 0; // 初期位置
   var amplitude = 100; // 上下の振幅（ピクセル）
@@ -48,13 +54,20 @@ export default {
   mounted() {
     // 描画領域1
     this.canvas = this.$refs.canvas1;
-    this.canvas.width = 400;
+    this.canvas.width = 200;
     this.canvas.height = 400;
     this.canvasCtx1 = this.canvas.getContext("2d");
 
     // 初期値設定
     position = this.canvas.height / 2; // 初期位置（画面の中央）
 
+    // グラフ描画領域1
+    Plotly.newPlot("graph", this.graphData, this.layout);
+
+  },
+  beforeRouteLeave(to, from, next) {
+    cancelAnimationFrame(this.requestId);
+    next();
   },
   data() {
     return {
@@ -65,7 +78,49 @@ export default {
       line: null,
       positionData: [], // 位置のデータ配列
       velocityData: [],
-      accelerationData: []
+      accelerationData: [],
+      graphData: [
+        {
+          x: [],
+          y: [],
+          mode: 'lines+markers',
+          name: 'Position',
+          marker: {
+            color: '#d7352b',
+            size: 1, // マーカーのサイズを設定
+          },
+        },
+        {
+          x: [],
+          y: [],
+          mode: 'lines+markers',
+          name: 'Velocity',
+          marker: {
+            color: '#ffa746',
+            size: 1, // マーカーのサイズを設定
+          },
+        },
+        {
+          x: [],
+          y: [],
+          mode: 'lines+markers',
+          name: 'Acceleration',
+          marker: {
+            color: '#5b97ee',
+            size: 1, // マーカーのサイズを設定
+          },
+        },
+      ],
+      graphLayout: {
+        xaxis: {
+          title: 'Time',
+        },
+        yaxis: {
+          title: 'Value',
+        }, 
+      },
+      requestId: null,
+      maxRunningTime: 10, // グラフの描画を停止する時間（秒）
     };
   },
   methods: {
@@ -75,9 +130,9 @@ export default {
       var elapsedTime = (Date.now() - startTime) / 1000; // 経過時間（秒）
 
       // データを更新
-      const cPosition = Math.sin(2 * elapsedTime); // 位置の計算（サイン波）
-      //const velocity = Math.cos(2 * elapsedTime); // 速度の計算（コサイン波）
-      //const acceleration = -Math.sin(2 * elapsedTime); // 加速度の計算（マイナスのサイン波）
+      const cPosition = - Math.sin(2 * elapsedTime); // 位置の計算（サイン波）
+      const velocity = - Math.cos(2 * elapsedTime); // 速度の計算（コサイン波）
+      const acceleration = Math.sin(2 * elapsedTime); // 加速度の計算（マイナスのサイン波）
 
       // 上下の振動を計算
       var y = position + amplitude * cPosition;
@@ -95,26 +150,29 @@ export default {
       this.canvasCtx1.arc(this.canvas.width / 2, y, 10, 0, 2 * Math.PI);
       this.canvasCtx1.stroke();
 
-      // 一定時間ごとにデータを更新
-      if (elapsedTime < 10) { // 10秒まで更新する場合
-        
-        setTimeout(this.animate, 10);
-      }else{
-        //
+      this.graphData[0].x.push(elapsedTime);
+      this.graphData[0].y.push(cPosition);
+      this.graphData[1].x.push(elapsedTime);
+      this.graphData[1].y.push(velocity);
+      this.graphData[2].x.push(elapsedTime);
+      this.graphData[2].y.push(acceleration);
+      Plotly.redraw("graph", this.graphData, this.layout);
+
+      this.requestId = requestAnimationFrame(this.animate);
+
+       // 一定時間ごとにデータを更新
+      if (elapsedTime > this.maxRunningTime) { 
+        cancelAnimationFrame(this.requestId);
       }
+
     },
     start() {
       // 時間初期化
       startTime = null;
-      this.drawChart(); // テスト
 
       // アニメーションの開始
       this.animate();
     },
-
-    drawChart() {
-      // 空
-    }
 
   }
 }
