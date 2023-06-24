bd63bd25034c9b14486b69d2d814e4205fc63a41
gometen <gometen@gmail.com>
2023-06-24 12:06:23 +0000

    Modified Canvas

--- src/components/GraphTest.vue
+++ src/components/GraphTest.vue
@@ -64,7 +64,7 @@
   
     // 物体の初期状態を設定
     var position = 0; // 初期位置
-    var amplitude = 100; // 上下の振幅（ピクセル）
+    var amplitude = 150; // 上下の振幅（ピクセル）
   
     var startTime = null;
   
@@ -72,7 +72,7 @@
     mounted() {
       // 描画領域1
       this.canvas = this.$refs.canvas1;
-      this.canvas.width = 200;
+      this.canvas.width = 100;
       this.canvas.height = 400;
       this.canvasCtx1 = this.canvas.getContext("2d");
   
@@ -162,9 +162,11 @@
         var elapsedTime = (Date.now() - startTime) / 1000; // 経過時間（秒）
   
         // データを更新
-        const cPosition = Math.sin(this.inputOmega * elapsedTime); // 位置の計算（サイン波）
-        const velocity = Math.cos(this.inputOmega * elapsedTime); // 速度の計算（コサイン波）
-        const acceleration = -Math.sin(this.inputOmega * elapsedTime); // 加速度の計算（マイナスのサイン波）
+        var thetaSpeed = this.inputOmega * elapsedTime
+        var theta = thetaSpeed % (2 * Math.PI);
+        const cPosition = Math.sin(theta); // 位置の計算（サイン波）
+        const velocity = Math.cos(theta); // 速度の計算（コサイン波）
+        const acceleration = -Math.sin(theta); // 加速度の計算（マイナスのサイン波）
   
         // 上下の振動を計算
         var y = position - amplitude * cPosition;
@@ -176,25 +178,70 @@
         // 物体を描画
         this.canvasCtx1.fillStyle = "rgb(0, 0, 0)";
         this.canvasCtx1.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.canvasCtx1.beginPath();
         this.canvasCtx1.lineWidth = 2;
         this.canvasCtx1.strokeStyle = "rgb(200, 50, 50)";
-        this.canvasCtx1.beginPath();
         //this.canvasCtx1.arc(this.canvas.width / 2, this.canvas.height / 2 - position, 10, 0, 2 * Math.PI);
         this.canvasCtx1.arc(this.canvas.width / 2, y, 10, 0, 2 * Math.PI);
         this.canvasCtx1.stroke();
+
+        // 高さを表す線
+        this.canvasCtx1.beginPath();
+        this.canvasCtx1.lineWidth = 1;
+        this.canvasCtx1.strokeStyle = "#D7352B";
+        this.canvasCtx1.moveTo(this.canvas.width / 2, this.canvas.height / 2);
+        this.canvasCtx1.lineTo(this.canvas.width / 2, y);
+        this.canvasCtx1.stroke();
   
         // 物体の回転を描画
+        // 物体の中心
+        var centerx = position + amplitude * Math.cos(theta);
+        var centery = position - amplitude * Math.sin(theta);
         this.canvasCtx2.fillStyle = "rgb(0, 0, 0)";
         this.canvasCtx2.fillRect(0, 0, this.canvas2.width, this.canvas2.height);
-        this.canvasCtx2.lineWidth = 2;
-        this.canvasCtx2.strokeStyle = "rgb(200, 50, 50)";
         this.canvasCtx2.beginPath();
+        this.canvasCtx2.lineWidth = 2;
+        this.canvasCtx2.strokeStyle = "#1F662E";
         this.canvasCtx2.arc(
-          position + amplitude * Math.cos(this.inputOmega * elapsedTime),
-          position - amplitude * Math.sin(this.inputOmega * elapsedTime),
+          centerx,
+          centery,
           10,
           0,
           2 * Math.PI);
+        this.canvasCtx2.stroke();
+
+        // 中心から円までの線
+        this.canvasCtx2.beginPath();
+        this.canvasCtx2.lineWidth = 2;
+        this.canvasCtx2.strokeStyle = "#1F662E";
+        this.canvasCtx2.moveTo(this.canvas2.width / 2, this.canvas2.height / 2);
+        this.canvasCtx2.lineTo(
+          centerx,
+          centery
+        );
+        this.canvasCtx2.stroke();
+
+        // 高さを表す線
+        this.canvasCtx2.beginPath();
+        this.canvasCtx2.lineWidth = 1;
+        this.canvasCtx2.strokeStyle = "#D7352B";
+        this.canvasCtx2.moveTo(centerx, this.canvas2.height / 2);
+        this.canvasCtx2.lineTo(
+          centerx,
+          centery
+        );
+        this.canvasCtx2.stroke();
+
+        // シータを表す弧
+        this.canvasCtx2.beginPath();
+        this.canvasCtx2.lineWidth = 1;
+        this.canvasCtx2.strokeStyle = "#F25A86";
+        this.canvasCtx2.arc(
+          this.canvas2.width / 2, 
+          this.canvas2.height / 2,
+          10,
+          0,
+          -theta, true);
         this.canvasCtx2.stroke();
   
         this.graphData[0].x.push(elapsedTime);
